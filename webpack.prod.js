f1e1ddae2d4d55e8068d25345eb50a3f59b1e6a4
alfacentaury <alfa.arnialfa@gmail.com>
2023-12-25 13:10:06 +0700

    Change version

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-//const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 
